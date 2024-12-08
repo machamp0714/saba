46273ae5811c7a9f0689455708a5c7ccf6b2fdcb
machamp0714 <machamp0714@gmail.com>
2024-12-08 16:50:32 +0900

    Add create http response struct

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -29,6 +29,74 @@ impl Header {
 
 impl HttpResponse {
     pub fn new(raw_response: String) -> Result<Self, Error> {
-        // pending...
+        // 先頭から空白を削除、行末を単一の改行に統一。\r はキャリッジリターン
+        let preprocessed_response = raw_response.trim_start().replace("\n\r", "\n");
+
+        // ステータスラインの分割
+        let (status_line, remaining) = match preprocessed_response.split_once("\n") {
+            Some((s, r)) => (s, r),
+            None => {
+                return Err(Error::Network(format!(
+                    "invalid http response: {}", preprocessed_response
+                )))
+            }
+        }
+
+        // ヘッダとボディの分割
+        let (headers, body) = match remaining.split_once("\n\n") {
+            Some((h, b)) => {
+                let mut headers = Vec::new();
+                for header in h.split("\n") {
+                    let splitted_header: Vec<&str> = header.splitn(2, ":").collect();
+                    headers.push(Header::new(
+                        String::from(splitted_header[0].trim()),
+                        String::from(splitted_header[1].trim())
+                    ));
+                }
+                (headers, b)
+            }
+            None => (Vec::new(), remaining)
+        }
+
+        // HttpResponse構造体を返す
+        let statuses: Vec<&str> = status_line.split(' ').collect();
+
+        Ok(Self {
+            version: statuses[0].to_string(),
+            status_code: statuses[1].to_string(),
+            reason: statuses[2].to_string(),
+            headers,
+            body: body.to_string(),
+        })
+    }
+
+    pub fn version(&self) -> String {
+        self.version.clone()
+    }
+
+    pub fn status_code(&self) -> u32 {
+        self.status_code
+    }
+
+    pub fn reason(&self) -> String [
+        self.reason.clone()
+    ]
+
+    pub fn headers(&self) -> Vec<Header> {
+        self.headers.clone()
+    }
+
+    pub fn body(&self) -> String {
+        self.body.clone()
+    }
+
+    pub fn header_value(&self, name: &str) -> Result<String, String> {
+        for h in &self.headers {
+            if (h.name == name) {
+                return Ok(h.value.clone());
+            }
+        }
+
+        Err(format!("failed to find {} in headers", name))
     }
 }
